cada86e233b4ed39342790e4b9c37c06c068433e
Barry-dE <barigbuenbira@gmail.com>
2024-01-12 20:01:31 +0100

    build features section

--- src/components/Features/Features.jsx
+++ src/components/Features/Features.jsx
@@ -1,5 +1,6 @@
 import "../../styles/components/features.scss";
 import GridOneSvg from "../../assets/Vector_features.svg";
+import arrowIcon from "../../assets/arrow.png";
 
 function Features() {
   return (
@@ -27,33 +28,93 @@ function Features() {
               <div className="features_grid_layout_wrapper">
                 <div className="features_grid_layout_item features_grid_layout_item--1">
                   <figure>
-                    <img src={GridOneSvg} alt="Grid background" width={400} />
+                    <img
+                      className="features_grid_background--1"
+                      src={GridOneSvg}
+                      alt="Grid background"
+                      width={400}
+                    />
                   </figure>
                   <div className="features_grid_content">
                     <div className="features_grid_content_wrapper">
-                      <p>
-                        Lorem ipsum dolor sit amet consectetur, adipisicing
-                        elit. Odit illum saepe, illo tempora molestiae voluptas
-                        atque repellat totam voluptatem perspiciatis veritatis
-                        adipisci nobis unde voluptatibus consequuntur architecto
-                        provident ex quaerat!
-                      </p>
+                      <div className="features_grid_content_box">
+                        <div className="features_grid_content_box_item features_grid_content_box_item--1">
+                          <span>
+                            <figure>
+                              <img
+                                src={arrowIcon}
+                                alt="arrow icon"
+                                width={300}
+                              />
+                            </figure>
+                          </span>
+                          <p>+ $22.15 Received from Nguyen Shane</p>
+                        </div>
+                        <div className="features_grid_content_box_item features_grid_content_box_item--2">
+                          <span>
+                            <figure>
+                              <img
+                                src={arrowIcon}
+                                alt="arrow icon"
+                                width={300}
+                              />
+                            </figure>
+                          </span>
+                          <p>+ $300.52 Received from Cooper Kristin</p>
+                        </div>
+                        <div className="features_grid_content_box_item features_grid_content_box_item--3">
+                          <span>
+                            <figure>
+                              <img
+                                src={arrowIcon}
+                                alt="arrow icon"
+                                width={300}
+                              />
+                            </figure>
+                          </span>
+                          <p> + $50.00 Received from Miles Esther</p>
+                        </div>
+                      </div>
+
+                      <div className="features_grid_content_box_item_title">
+                        <h3>Seamless Payments</h3>
+                        <p>
+                          <span>
+                            Enjoy secure, seamless transactions that make
+                            managing your money a
+                          </span>
+                          <span>breeze.</span>
+                        </p>
+                      </div>
                     </div>
                   </div>
                 </div>
                 <div className="features_grid_layout_item features_grid_layout_item--2">
                   <figure>
-                    <img src={GridOneSvg} alt="Grid background" width={400} />
+                    <img
+                      className="features_grid_background--2"
+                      src={GridOneSvg}
+                      alt="Grid background"
+                      width={400}
+                    />
                   </figure>
                 </div>
                 <div className="features_grid_layout_item features_grid_layout_item--3">
                   <figure>
-                    <img src={GridOneSvg} alt="Grid background" />
+                    <img
+                      className="features_grid_background--3"
+                      src={GridOneSvg}
+                      alt="Grid background"
+                    />
                   </figure>
                 </div>
                 <div className="features_grid_layout_item features_grid_layout_item--4">
                   <figure>
-                    <img src={GridOneSvg} alt="Grid background" width={400} />
+                    <img
+                      className="features_grid_background--4"
+                      src={GridOneSvg}
+                      alt="Grid background"
+                    />
                   </figure>
                 </div>
               </div>
