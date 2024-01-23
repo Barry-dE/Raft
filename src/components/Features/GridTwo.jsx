4abb386806e39e0c2bd7c8f494e8a4158658331e
Barigbue <barigbuenbira@gmail.com>
2024-01-23 15:19:32 +0100

    Build features

--- src/components/Features/GridTwo.jsx
+++ src/components/Features/GridTwo.jsx
@@ -4,7 +4,7 @@ import crypto from '../../assets/crypto.svg';
 function GridTwo() {
     return (
         <div className="features_grid_layout_item features_grid_layout_item--2">
-            <figure>
+            <figure className="features_grid_layout_item_grid">
                 <img
                     className="features_grid_background--2"
                     src={GridOneSvg}
@@ -14,44 +14,53 @@ function GridTwo() {
             </figure>
             <div className="features_grid_content_investing">
                 <div className="features_grid_content_investing_wrapper">
-                    <div className="features_grid_contents_content  features_grid_contents_content--1">
-                        <figure className="features_grid_contents_content_svg ">
-                            <img
-                                className="features_grid_contents_content_svg_icon"
-                                src={crypto}
-                                alt="cryptocurrency"
-                            />
-                        </figure>
-                        <p>Cryptocurrency</p>
+                    <div className="features_grid_contents_content_wrapper">
+                        <div className="features_grid_contents_content  features_grid_contents_content--1">
+                            <figure className="features_grid_contents_content_svg ">
+                                <img
+                                    className="features_grid_contents_content_svg_icon"
+                                    src={crypto}
+                                    alt="cryptocurrency"
+                                />
+                            </figure>
+                            <p>Cryptocurrency</p>
+                        </div>
+                        <div className="features_grid_contents_content">
+                            <figure className="features_grid_contents_content_svg ">
+                                <img
+                                    className="features_grid_contents_content_svg_icon"
+                                    src={crypto}
+                                    alt="cryptocurrency"
+                                />
+                            </figure>
+                            <p>Smart Portfolio Investing</p>
+                        </div>
+                        <div className="features_grid_contents_content">
+                            <figure className="features_grid_contents_content_svg ">
+                                <img
+                                    className="features_grid_contents_content_svg_icon"
+                                    src={crypto}
+                                    alt="cryptocurrency"
+                                />
+                            </figure>
+                            <p>Sustainable Investing</p>
+                        </div>
                     </div>
-                    <div className="features_grid_contents_content">
-                        <figure className="features_grid_contents_content_svg ">
-                            <img
-                                className="features_grid_contents_content_svg_icon"
-                                src={crypto}
-                                alt="cryptocurrency"
-                            />
-                        </figure>
-                        <p>Smart Portfolio Investing</p>
+                    <div className="features_grid_overlay"></div>
+                    <div className="smart_investing">
+                        <h3 className="smart_investing_title">
+                            Smart Investing
+                        </h3>
+                        <p className="smart_investing_description">
+                            <span>
+                                Grow your wealth confidently with our
+                                personalized investment
+                            </span>
+                            <span>
+                                solutions, tailored to your financial goals.
+                            </span>
+                        </p>
                     </div>
-                    <div className="features_grid_contents_content">
-                        <figure className="features_grid_contents_content_svg ">
-                            <img
-                                className="features_grid_contents_content_svg_icon"
-                                src={crypto}
-                                alt="cryptocurrency"
-                            />
-                        </figure>
-                        <p>Sustainable Investing</p>
-                    </div>
-                </div>
-                <div className="features_grid_overlay"></div>
-                <div className="smart_investing">
-                    <h3 className="smart_investing_title">Smart Investing</h3>
-                    <p className="smart_investing_description">
-                        Grow your wealth confidently with our personalized
-                        investment solutions, tailored to your financial goals.
-                    </p>
                 </div>
             </div>
         </div>
