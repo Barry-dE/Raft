9bd5b833c4e2c7acc5a5155f4cf49a5200ca061b
Barigbue <barigbuenbira@gmail.com>
2024-02-26 02:15:57 +0100

    change hero content bacgtound

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import { IoChevronForward } from 'react-icons/io5';
-import gridBackgtound from '../../assets/Grid.svg';
+import gridBackground from '/assets/Grid.svg';
 import '/src/styles/components/hero.scss';
 
 function Hero() {
@@ -7,6 +7,15 @@ function Hero() {
         <section className="hero">
             <div className="hero__Wrapper">
                 <div className="hero__content">
+                    <div className="hero__content__gridBackground">
+                        <figure>
+                            <img
+                                className="hero__content__gridBackground__image"
+                                src={gridBackground}
+                                alt="grid background"
+                            />
+                        </figure>
+                    </div>
                     <div className="hero__content__wrapper">
                         <div className="hero__content__marquee">
                             <div className="hero__content__marquee__wrapper">
@@ -18,19 +27,17 @@ function Hero() {
                         </div>
                         <div className="hero__content__title">
                             <div className="hero__content__title_text">
-                                <h1>Building the future of </h1>
-                                <h1>banking</h1>
+                                <p>Building the future of </p>
+                                <p>banking</p>
                             </div>
                         </div>
                         <div className="hero__content__title__description">
                             <div className="hero__content__title__description__text">
                                 <p>
                                     Experience the future of banking with RAFT.
+                                    We are here to
                                 </p>
-                                <p>
-                                    We are here to empower your financial
-                                    journey.
-                                </p>
+                                <p>empower your financial journey.</p>
                             </div>
                         </div>
                         <button className="hero__content__button">
