872d43e71c3fb35bc02cb6d1c8f43c32cb7b6d9b
Barigbue <barigbuenbira@gmail.com>
2024-03-05 00:06:54 +0100

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Benefits from './components/Benefits/Benefits';
 import Companies from './components/Companies/Companies';
 import Services from './components/Features/Features';
 import Fees from './components/Fees/Fees';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 import Nav from './components/Nav/Nav';
@@ -17,6 +18,7 @@ function App() {
             <Services />
             <Fees />
             <Benefits />
+            <Footer />
         </>
     );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,95 @@
+import '../../styles/components/footer.scss';
+import { IoChevronForward } from 'react-icons/io5';
+import logoImage from '/assets/logo.svg';
+
+function Footer() {
+    return (
+        <footer className="footer">
+            <div className="footer__wrapper">
+                <div className="footer__header">
+                    <p className="footer__header__text">
+                        Frequently asked questions
+                    </p>
+                </div>
+                <div className="footer__questions">
+                    <div className="footer__question">
+                        <p>How do I create an account with RAFT?</p>
+                        <IoChevronForward className="footer__question__icon" />
+                    </div>
+                    <div className="footer__question">
+                        <p>
+                            How does RAFT ensure the security of my financial
+                            data?
+                        </p>
+                        <IoChevronForward className="footer__question__icon" />
+                    </div>
+                    <div className="footer__question">
+                        <p>
+                            What types of transactions can I perform with RAFT?
+                        </p>
+                        <IoChevronForward className="footer__question__icon" />
+                    </div>
+                    <div className="footer__question">
+                        <p>
+                            What benefits does RAFT offer for wealth management?
+                        </p>
+                        <IoChevronForward className="footer__question__icon" />
+                    </div>
+                </div>
+                <div className="footer__info">
+                    <div className="footer__info__wrapper">
+                        <div className="footer__info__logo">
+                            <figure>
+                                <img src={logoImage} alt="" />
+                            </figure>
+                            <p>Raft</p>
+                        </div>
+                        <div className="footer__info__items">
+                            <div className="footer__info__item">
+                                <div className=" footer__info__item__qrcode">
+                                    qr code
+                                </div>
+                                <div className="footer__info__item__info">
+                                    <div className="footer__Info__item__info__content">
+                                        <h3>About Us</h3>
+                                        <div className="footer__info__Item__content__text">
+                                            <p>Our Company</p>
+                                            <p>Careers</p>
+                                            <p>Press Kits</p>
+                                        </div>
+                                    </div>
+                                    <div className="footer__Info__item__info__content">
+                                        <h3>Legal</h3>
+                                        <div className="footer__info__Item__content__text">
+                                            <p>Terms of use</p>
+                                            <p>Privacy Policy</p>
+                                            <p>About Us</p>
+                                        </div>
+                                    </div>
+                                    <div className="footer__Info__item__info__content">
+                                        <h3>Support</h3>
+                                        <div className="footer__info__Item__content__text">
+                                            <p>Contact Us</p>
+                                            <p>FAQ</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="footer__info__item">
+                                <div className="footer__info__item__locale">
+                                    <p>English United Kingdom</p>
+                                    <IoChevronForward className="footer__question__icon" />
+                                </div>
+                                <div className="footer__info__item__company">
+                                    <p>C Raft Corp, LLC.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
