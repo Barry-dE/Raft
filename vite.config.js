406274dd5b011be9e93474dae1bbf9e7f4d01c5a
Barigbue <barigbuenbira@gmail.com>
2024-03-09 16:46:17 +0100

    changes to views

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,25 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "./",
-  resolve: {
-    extensions: [".js", ".jsx", ".json", ".png"],
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
+    plugins: [react()],
+    base: './',
+    resolve: {
+        extensions: ['.js', '.jsx', '.json', '.png'],
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                additionalData: `
           @import './src/styles/abstract/_mixins.scss';
           @import './src/styles/abstract/_variables.scss';
           @import './src/styles/shared/description.scss';
           @import './src/styles/shared/titles.scss';
           @import './src/styles/shared/buttons.scss';
+                    @import './src/styles/utils/responsive.scss';
         `,
-      },
+            },
+        },
     },
-  },
 });
